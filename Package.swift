78b4a9251cc4a835b3045f223e6fa3d17adb91f7
sai <330770597@qq.com>
2023-09-20 15:20:44 +0800

    macos build

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ import PackageDescription
 let package = Package(
     name: "Libyuv",
     platforms: [
-        .macOS(.v10_14), .iOS(.v13)
+        .macOS(.v12), .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
